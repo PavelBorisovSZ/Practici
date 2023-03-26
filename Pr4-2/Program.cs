a9adaa1f841597d8e61c8bff08e5e3e5e8457759
PavelBorisovSZ <PavelBorisovKot@yandex.ru>
2023-03-26 15:17:38 +0300

    PR4

---
+++ Pr4-2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr4_2
+{
+    class Packag
+    {
+        public string Descript
+        {
+            get;
+            private set;
+        }
+        public int Weight
+        {
+            get;
+            private set;
+        }
+        public Packag(string description, int weight)
+        {
+            Descript = description;
+            Weight = weight;
+        }
+    }
+    class SendingService
+    {
+        private static int LimitWeight;
+        private const int Limit = 10;
+        public void SendPackag(Packag packag)
+        {
+            LimitWeight += packag.Weight;
+            if (LimitWeight >= Limit)
+            {
+                Console.WriteLine("Ошибка! Вес отправленных посылок превысил лимит, отправка прервана.");
+            }
+            else
+            {
+                Console.WriteLine("{0} весом {1} кг успешно отправлена.", packag.Descript, packag.Weight);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Packag packag_1 = new Packag("Стандартная посылка №0000001", 4);
+            Packag packag_2 = new Packag("Тяжеловесная посылка №0000002", 7);
+            SendingService sendingService = new SendingService();
+            sendingService.SendPackag(packag_1);
+            sendingService.SendPackag(packag_2);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Pr4-3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr4_3
+{
+    class Orc
+    {
+        private static int Counter;
+        public static int amountGoldAll
+        {
+            get;
+            private set;
+        }
+        public int amountGold
+        {
+            get;
+            private set;
+        }
+        public Orc()
+        {
+            Counter++;
+            amountGoldAll += 2;
+            if (Counter > 5)
+            {
+                amountGold += 2;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Orc ors_1 = new Orc();
+            Console.WriteLine(Orc.amountGoldAll);
+            Orc ors_2 = new Orc();
+            Orc ors_3 = new Orc();
+            Console.WriteLine(Orc.amountGoldAll);
+            Orc ors_4 = new Orc();
+            Orc ors_5 = new Orc();
+            Orc ors_6 = new Orc();
+            Console.WriteLine(ors_6.amountGold);
+            Console.ReadKey(true);
+        }
+    }
+}
