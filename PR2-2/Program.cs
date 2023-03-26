7ca337f94846c7b4bbf84491f81f4433dca7192b
PavelBorisovSZ <PavelBorisovKot@yandex.ru>
2023-03-26 14:53:07 +0300

    PR2

---
+++ PR2-2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr2_2
+{
+    class Num
+    {
+        private int numb = 0;
+        public int GetNumber(int number)
+        {
+            if (SetNumber(number))
+            {
+                this.numb = number;
+            }
+            else
+            {
+                this.numb = 0;
+            }
+            return this.numb;
+        }
+        private bool SetNumber(int number)
+        {
+            if (number == this.numb + 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Num num = new Num();
+            int number;
+            for (int i = 1; i >= 0; i = num.GetNumber(number) + 1)
+            {
+                Console.WriteLine($"Введите число {i}");
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ PR2-3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr2_3
+{
+    internal class Program
+    {
+        class Cr
+        {
+            private string s;
+            private int shift;
+            public string GetCipher()
+            {
+                return s;
+            }
+            public void SetCipher(string code, int shift)
+            {
+                this.s = EncryptStr(code, shift);
+                this.shift = shift;
+            }
+            private string EncryptStr(string code, int shift)
+            {
+                char[] alphabet = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
+                char L;
+                int index;
+                string stringCipher = "";
+                for (int i = 0; i < code.Length; i++)
+                {
+
+                    L = code[i];
+                    if (L == ' ')
+                    {
+                        stringCipher += L;
+                        continue;
+                    }
+                    index = Array.IndexOf(alphabet, L);
+                    index += shift;
+                    if (index > 32)
+                    {
+                        index -= 33;
+                    }
+                    else if (index < 0)
+                    {
+                        index += 33;
+                    }
+                    stringCipher += alphabet[index];
+                }
+                return stringCipher;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Cr myCipher = new Cr();
+            myCipher.SetCipher("в своем друге ты должен иметь своего лучшего врага", 21);
+            Console.WriteLine(myCipher.GetCipher());
+            myCipher.SetCipher("мрупщл нужнлрю ц мрупщр", -12);
+            Console.WriteLine(myCipher.GetCipher());
+            Console.ReadKey(true);
+        }
+    }
+}
